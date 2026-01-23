e60148b0dd80b71e3d2698ea17bff92497fb359f
christianbalibad <christianbalibad@gmail.com>
2026-01-23 14:00:22 +0800

    add: ACTIVITY 3

---
+++ ACTIVITY 3/UndefinedShoes/Models/Shoe.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace UndefinedShoes.Models
+{
+    public class Shoe
+    {
+        public int Id { get; set; }
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public string? Style { get; set; }
+        public string? Color { get; set; }
+        public int Size { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
